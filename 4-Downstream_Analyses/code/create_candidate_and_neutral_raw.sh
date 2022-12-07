b24aa6322f21c778199da03c9cecc651e328fc65
Enricobazzi <enricobazzical@gmail.com>
2022-12-07 16:42:43 +0100

    script that creates raw files for candidate SNPs dataset and neutral SNPs dataset (no genes no baypass outliers no rda outliers)

---
+++ 4-Downstream_Analyses/code/create_candidate_and_neutral_raw.sh
@@ -0,0 +1,40 @@
+# on genomics-a
+cd /home/ebazzicalupo/Local_adaptation_Eurasian_lynx
+
+# define vcf from which to extract candidate and neutral SNPs
+sd_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.sd_cand.vcf
+qvals_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.qvals_cand.vcf
+vcf_neutral=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.neutral.vcf
+
+# I have a samplestoremove.txt that includes samples I want to remove from the analysis:
+# cat samplestoremove.txt
+# # c_ll_ba_0216 c_ll_ba_0216 0 0 0 -9
+# # c_ll_ba_0233 c_ll_ba_0233 0 0 0 -9
+# # c_ll_cr_0211 c_ll_cr_0211 0 0 0 -9
+# # h_ll_ba_0214 h_ll_ba_0214 0 0 0 -9
+# # h_ll_ba_0215 h_ll_ba_0215 0 0 0 -9
+
+# sd candidate snps:
+plink_1.9 --vcf $sd_cand_vcf \
+ --double-id --allow-extra-chr --set-missing-var-ids @:# \
+ --remove 4-Downstream_Analyses/tables/samplestoremove.txt --geno 0 \
+ --recode A --out 4-Downstream_Analyses/tables/sd_candidate_snps
+
+# qvals candidate snps:
+plink_1.9 --vcf $qvals_cand_vcf \
+ --double-id --allow-extra-chr --set-missing-var-ids @:# \
+ --remove 4-Downstream_Analyses/tables/samplestoremove.txt --geno 0 \
+ --recode A --out 4-Downstream_Analyses/tables/qvals_candidate_snps
+ 
+# neutral snps - prune first:
+plink_1.9 --vcf $vcf_neutral \
+--double-id --allow-extra-chr --set-missing-var-ids @:# --geno 0 --maf 0.05 \
+--remove 4-Downstream_Analyses/tables/samplestoremove.txt --indep 100 10 2 \
+--out 4-Downstream_Analyses/tables/neutral_pruned
+# neutral snps:
+plink_1.9 --vcf $vcf_neutral \
+ --double-id --allow-extra-chr --set-missing-var-ids @:# \
+ --remove 4-Downstream_Analyses/tables/samplestoremove.txt \
+ --extract <(shuf --random-source=<(yes 42) 4-Downstream_Analyses/tables/neutral_pruned.prune.in | head -n 10000) \
+ --recode A --out 4-Downstream_Analyses/tables/neutral_snps
+
