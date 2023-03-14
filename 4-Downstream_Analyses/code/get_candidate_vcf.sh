6efc603c3da66431796c090c6ddb166e0de75b90
Enricobazzi <enricobazzical@gmail.com>
2023-03-14 13:19:55 +0100

    generalized to apply to different predictors and conditions

--- 4-Downstream_Analyses/code/get_candidate_vcf.sh
+++ 4-Downstream_Analyses/code/get_candidate_vcf.sh
@@ -1,20 +1,24 @@
+#!/bin/bash
+
 # to get the vcf of candidate SNPs I can intersect the vcf with the bed of 
 # candidate snps
 vcf=1-Preparing_Genetic_Data/tables/ll_wholegenome_LyCa_ref.sorted.filter7.vcf
 
-sd_cand_bed=3-Identifying_Candidate_Loci/tables/overlap_sd_snps_nodup.bed
-sd_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.sd_cand.vcf
+sd_cand_bed=3-Identifying_Candidate_Loci/tables/${1}.overlap_sd_snps_nodup.bed
+sd_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.${1}.sd_cand.vcf
 
-qvals_cand_bed=3-Identifying_Candidate_Loci/tables/overlap_qvals_snps_nodup.bed
-qvals_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.qvals_cand.vcf
+qvals_cand_bed=3-Identifying_Candidate_Loci/tables/${1}.overlap_qvals_snps_nodup.bed
+qvals_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.${1}.qvals_cand.vcf
 
 # sd candidates:
+echo "extracting sd candidate vcf for ${1}"
 bedtools intersect -header \
     -a $vcf \
     -b $sd_cand_bed \
     > $sd_cand_vcf
 
 # qvals candidates:
+echo "extracting qvals candidate vcf for ${1}"
 bedtools intersect -header \
     -a $vcf \
     -b $qvals_cand_bed \
