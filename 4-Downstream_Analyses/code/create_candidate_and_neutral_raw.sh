514e785d2c19582541ace5b319f887206d18655b
Enricobazzi <enricobazzical@gmail.com>
2023-03-14 13:18:28 +0100

    generalized to apply to different predictors and conditions

--- 4-Downstream_Analyses/code/create_candidate_and_neutral_raw.sh
+++ 4-Downstream_Analyses/code/create_candidate_and_neutral_raw.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 # on genomics-a
 cd /home/ebazzicalupo/Local_adaptation_Eurasian_lynx
 
 # define vcf from which to extract candidate and neutral SNPs
-sd_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.sd_cand.vcf
-qvals_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.qvals_cand.vcf
-vcf_neutral=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.neutral.vcf
+sd_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.${1}.sd_cand.vcf
+qvals_cand_vcf=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.${1}.qvals_cand.vcf
+vcf_neutral=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.${1}.neutral.vcf
 
 # I have a samplestoremove.txt that includes samples I want to remove from the analysis:
 # cat samplestoremove.txt
@@ -15,26 +17,29 @@ vcf_neutral=4-Downstream_Analyses/tables/ll_wholegenome_LyCa_ref.sorted.filter7.
 # # h_ll_ba_0215 h_ll_ba_0215 0 0 0 -9
 
 # sd candidate snps:
+echo "extracting sd candidate raw for ${1}"
 plink_1.9 --vcf $sd_cand_vcf \
  --double-id --allow-extra-chr --set-missing-var-ids @:# \
  --remove 4-Downstream_Analyses/tables/samplestoremove.txt --geno 0 \
- --recode A --out 4-Downstream_Analyses/tables/sd_candidate_snps
+ --recode A --out 4-Downstream_Analyses/tables/${1}.sd_candidate_snps
 
 # qvals candidate snps:
+echo "extracting qvals candidate raw for ${1}"
 plink_1.9 --vcf $qvals_cand_vcf \
  --double-id --allow-extra-chr --set-missing-var-ids @:# \
  --remove 4-Downstream_Analyses/tables/samplestoremove.txt --geno 0 \
- --recode A --out 4-Downstream_Analyses/tables/qvals_candidate_snps
+ --recode A --out 4-Downstream_Analyses/tables/${1}.qvals_candidate_snps
  
 # neutral snps - prune first:
+echo "extracting neutral raw for ${1}"
 plink_1.9 --vcf $vcf_neutral \
 --double-id --allow-extra-chr --set-missing-var-ids @:# --geno 0 --maf 0.05 \
 --remove 4-Downstream_Analyses/tables/samplestoremove.txt --indep 100 10 2 \
---out 4-Downstream_Analyses/tables/neutral_pruned
+--out 4-Downstream_Analyses/tables/${1}.neutral_pruned
 # neutral snps:
 plink_1.9 --vcf $vcf_neutral \
  --double-id --allow-extra-chr --set-missing-var-ids @:# \
  --remove 4-Downstream_Analyses/tables/samplestoremove.txt \
  --extract <(shuf --random-source=<(yes 42) 4-Downstream_Analyses/tables/neutral_pruned.prune.in | head -n 10000) \
- --recode A --out 4-Downstream_Analyses/tables/neutral_snps
+ --recode A --out 4-Downstream_Analyses/tables/${1}.neutral_snps
 
